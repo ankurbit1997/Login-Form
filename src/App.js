4f48101a2b4fb315dafbb2e1b386975e15e7f5dc
ankurbit1997 <31044900+ankurbit1997@users.noreply.github.com>
2021-10-09 17:05:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Error from "./pages/Error";
-import Home from "./pages/Home";
-import Launches from "./pages/Launches";
-import RocketPage from "./pages/rocket";
 
-const client = new ApolloClient({
-  uri: "https://api.spacex.land/graphql",
-  cache: new InMemoryCache(),
-});
+import "./style.scss";
 
 const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/launches" component={Launches} />
-          <Route exact path="/rocket/:id" component={RocketPage} />
-          <Route component={Error} />
-        </Switch>
-      </Router>
-    </ApolloProvider>
-  );
+  return <div>App</div>;
 };
 
 export default App;
